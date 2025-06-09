320d5c257c280b165ad16c354d1ac7db9c9cecd1
rayanegouda <Kameha13>
2025-06-09 22:53:58 +0200

    maj

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
 aws_config = Config(
-    max_pool_connections=100,  # Augmente le pool de connexions
-    retries={'max_attempts': 3}
+	max_pool_connections=100,  # Augmente le pool de connexions
+	retries={'max_attempts': 3}
 )
+
 
 def get_secret_value(secret_id: str):
 	region_name = os.environ.get("AWS_REGION_NAME")
@@ -23,76 +24,79 @@ def get_secret_value(secret_id: str):
 	response = client.get_secret_value(SecretId=secret_id)
 	return json.loads(response["SecretString"])
 
-def get_db_credentials(region="eu-north-1"):
-    # Changer ici les IDs exacts de secrets pour chaque champ
-    secret_ids = {
-        "host": "rds-db-credentials/cluster-3MGGV2VUZDWQSJFDD6TQ4744HQ/admin/1748251685700",
-        "port": "rds-db-port-secret-id",  # optionnel, ou hardcodé 3306
-        "username": "rds!cluster-27e3f900-f4c4-44bc-a1e0-19cc44356684",
-        "password": "rds!cluster-27e3f900-f4c4-44bc-a1e0-19cc44356684",
-        "dbname": "rds-db-name-secret-id"
-    }
 
-    return {
-        "host": get_secret_value(secret_ids["host"])["host"],
-        "port": int(3306),
-        "user": get_secret_value(secret_ids["username"])["username"],
-        "password": get_secret_value(secret_ids["password"])["password"],
-        "dbname": "guacamole_db"
-    }
+def get_db_credentials(region="eu-north-1"):
+	# Changer ici les IDs exacts de secrets pour chaque champ
+	secret_ids = {
+		"host": "rds-db-credentials/cluster-3MGGV2VUZDWQSJFDD6TQ4744HQ/admin/1748251685700",
+		"port": "rds-db-port-secret-id",  # optionnel, ou hardcodé 3306
+		"username": "rds!cluster-27e3f900-f4c4-44bc-a1e0-19cc44356684",
+		"password": "rds!cluster-27e3f900-f4c4-44bc-a1e0-19cc44356684",
+		"dbname": "rds-db-name-secret-id"
+	}
+
+	return {
+		"host": get_secret_value(secret_ids["host"])["host"],
+		"port": int(3306),
+		"user": get_secret_value(secret_ids["username"])["username"],
+		"password": get_secret_value(secret_ids["password"])["password"],
+		"dbname": "guacamole_db"
+	}
+
 
 def create_db_engine():
-    creds = get_db_credentials()
-    db_url = f"mysql+pymysql://{creds['user']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['dbname']}"
-    return create_engine(db_url, pool_pre_ping=True)
+	creds = get_db_credentials()
+	db_url = f"mysql+pymysql://{creds['user']}:{creds['password']}@{creds['host']}:{creds['port']}/{creds['dbname']}"
+	return create_engine(db_url, pool_pre_ping=True)
+
 
 @app.route('/create-connection', methods=['POST'])
 def create_connection():
-    data = request.json
-    ip = data.get("ip")
-    private_key = data.get("private_key")
+	data = request.json
+	ip = data.get("ip")
+	private_key = data.get("private_key")
 
-    if not ip or not private_key:
-        return jsonify({"error": "Missing IP or private_key"}), 400
-	#region_name = os.environ.get("REGION_NAME")
-	#aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-	#aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    engine = create_db_engine()
-    conn_name = f"SSH - {ip}"
-    try:
-        with engine.begin() as conn:
-            # Insertion de la connexion
-            conn.execute(text("""
+	if not ip or not private_key:
+		return jsonify({"error": "Missing IP or private_key"}), 400
+	# region_name = os.environ.get("REGION_NAME")
+	# aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+	# aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+	engine = create_db_engine()
+	conn_name = f"SSH - {ip}"
+	try:
+		with engine.begin() as conn:
+			# Insertion de la connexion
+			conn.execute(text("""
                 INSERT INTO guacamole_connection (connection_name, protocol, parent_id)
                 VALUES (:name, 'ssh', NULL)
             """), {"name": conn_name})
 
-            # Récupération du dernier ID
-            result = conn.execute(text("SELECT LAST_INSERT_ID() AS id"))
-            connection_id = result.fetchone()["id"]
+			# Récupération du dernier ID
+			result = conn.execute(text("SELECT LAST_INSERT_ID() AS id")).mappings()
+			connection_id = result.fetchone()["id"]
 
-            # Insertion des paramètres
-            parameters = [
-                ("hostname", ip),
-                ("port", "22"),
-                ("username", "ubuntu"),
-                ("private-key", private_key)
-            ]
+		# Insertion des paramètres
+		parameters = [
+			("hostname", ip),
+			("port", "22"),
+			("username", "ubuntu"),
+			("private-key", private_key)
+		]
 
-            for name, value in parameters:
-                conn.execute(text("""
+		for name, value in parameters:
+			conn.execute(text("""
                     INSERT INTO guacamole_connection_parameter
                     (connection_id, parameter_name, parameter_value)
                     VALUES (:cid, :pname, :pvalue)
                 """), {"cid": connection_id, "pname": name, "pvalue": value})
 
-        return jsonify({
-            "connection_id": connection_id,
-            "connection_name": conn_name
-        }), 201
+	return jsonify({
+		"connection_id": connection_id,
+		"connection_name": conn_name
+	}), 201
 
-    except SQLAlchemyError as e:
-        return jsonify({"error": str(e)}), 500
+except SQLAlchemyError as e:
+return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
